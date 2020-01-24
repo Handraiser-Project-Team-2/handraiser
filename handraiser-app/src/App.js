1fbb89d5af7b52cb47e81d32d6a583e60d4dc62f
Christian Ray Frecia <christian.frecia@boom.camp>
2020-01-24 10:48:08 +0800

    Initial UI only

---
+++ handraiser-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Welcome to Handraiser Project
+      </Button>
+    </div>
+  );
+}
