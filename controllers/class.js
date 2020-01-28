7d82d36b866fe72c28256b903c0ed1bf1c8c9a6e
lyzamaemirabete <lyza.mirabete@boom.camp>
2020-01-28 14:24:48 +0800

    modified

--- controllers/class.js
+++ controllers/class.js
@@ -4,15 +4,15 @@ function getStudentsByClass(req, res) {
 
   db.query(
     `SELECT
-        user_profile.profile_id,
-        user_profile.first_name,
-        user_profile.last_name,
-        user_profile.middle_name,
-        user_profile.image
-      FROM
-        user_profile
-      INNER JOIN users ON users.profile_id = user_profile.profile_id
-      INNER JOIN classroom ON users.user_id = classroom.user_id where user_type_id=3 and class_id = ${class_id};`
+      user_profile.profile_id,
+      user_profile.first_name,
+      user_profile.last_name,
+      user_profile.middle_name,
+      user_profile.image
+    FROM
+      user_profile
+    INNER JOIN users ON users.profile_id = user_profile.profile_id
+    INNER JOIN classroom ON users.user_id = classroom.user_id where user_type_id=3 and class_id = ${class_id};`
   )
     .then(students => res.status(201).send(students))
     .catch(err => {
