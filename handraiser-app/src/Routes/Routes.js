80c2b69e0b9481a3e56f91e23faf261fc295eccc
Christian Ray Frecia <christian.frecia@boom.camp>
2020-01-29 15:02:47 +0800

    Initial class view

--- handraiser-app/src/Routes/Routes.js
+++ handraiser-app/src/Routes/Routes.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Login from "../components/Login/Login";
 import Student from "../components/student/StudentUi";
 import SuperAdmin from "../components/Super-Admin/Super-Admin";
+import Class from '../components/Class/ClassLanding'
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
       <Route path="/superadmin" component={SuperAdmin} />
       <Route path="/admin" />
       <Route path="/student" component={Student} />
+      <Route path="/class" component={Class} />
       <Route path="/mentor" />
     </BrowserRouter>
   );
