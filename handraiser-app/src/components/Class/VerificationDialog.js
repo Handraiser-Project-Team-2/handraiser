4f5d7e63435ad2c3761fa1bea9952e9c81741e40
Christian Ray Frecia <christian.frecia@boom.camp>
2020-01-31 11:58:09 +0800

    Added Verify Input Masking

--- handraiser-app/src/components/Class/VerificationDialog.js
+++ handraiser-app/src/components/Class/VerificationDialog.js
@@ -7,11 +7,49 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
+import PropTypes from "prop-types";
+import MaskedInput from "react-text-mask";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+
 import MuiAlert from "@material-ui/lab/Alert";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
+
+function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={ref => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        "-",
+        /[A-Za-z0-9]/,
+        /[A-Za-z0-9]/,
+        "-",
+        /[A-Za-z0-9]/
+      ]}
+      placeholderChar={"\u2000"}
+    />
+  );
+}
+
+TextMaskCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired
+};
 
 export default function VerificationDialog() {
   const [open, setOpen] = React.useState(true);
@@ -26,19 +64,19 @@ export default function VerificationDialog() {
 
   const [input, setInput] = useState("");
   const handleChange = e => {
-    console.log(e.target.value);
     setInput(e.target.value);
-    if(e.target.value.length === 13){
-      setValid(null)
+    if (e.target.value.length === 13) {
+      setValid(null);
     }
   };
 
   const [valid, setValid] = useState(null);
   const handleSubmit = () => {
-    if (input === `` || input.length < 13) {
+    if (/\s/.test(input) || input === ``) {
       setValid(true);
     } else {
       handleClose();
+      setInput('')
     }
   };
 
@@ -56,16 +94,15 @@ export default function VerificationDialog() {
             To access this site, please enter your verification key provided by
             your administrator.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Verification Code"
-            fullWidth
-            name="validation_key"
-            defaultValue={input}
+          <InputLabel htmlFor="formatted-text-mask-input">
+            Verification Code
+          </InputLabel>
+          <Input
+            value={input}
             onChange={e => handleChange(e)}
-            inputProps={{ maxLength: 13 }}
+            inputComponent={TextMaskCustom}
+            fullWidth
+            autoFocus
           />
         </DialogContent>
         <DialogActions>
