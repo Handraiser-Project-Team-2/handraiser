714597a6c0b1d23afa3f7f6793449f585e8fbbf7
Christian Ray Frecia <christian.frecia@boom.camp>
2020-02-04 12:17:32 +0800

    Remove warnings

--- handraiser-app/src/components/Class/ClassLanding.js
+++ handraiser-app/src/components/Class/ClassLanding.js
@@ -1,12 +1,4 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
@@ -49,8 +41,6 @@ let socket;
 export default function ClassLanding() {
   let token = sessionStorage.getItem("token").split(" ")[1];
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
 
 
   const ENDPOINT = "localhost:5000";
@@ -60,13 +50,6 @@ export default function ClassLanding() {
   }, []);
   // const theme = useTheme();
   // const matches = useMediaQuery(theme.breakpoints.up("md"));
-  const handleMenu = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   useEffect(() => {
     fetchUserData();
