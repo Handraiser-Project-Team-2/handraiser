a27d1e6e850d4eddaaa544b4022d7fad66e43640
JeffreyMolleno <jeffrey.molleno.21@gmail.com>
2020-02-05 01:54:09 +0800

    Added verification and reconfigured class landing page in responce to active user type

--- handraiser-app/src/components/Class/ClassLanding.js
+++ handraiser-app/src/components/Class/ClassLanding.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
 
-
-import io from "socket.io-client";
 // import { useTheme } from "@material-ui/core/styles";
 // import useMediaQuery from "@material-ui/core/useMediaQuery";
-
 
 // COMPONENTS
 import CardPage from "./CardPage";
 import FindClassDialog from "./FindClassDialog";
 import VerificationDialog from "./VerificationDialog";
-
 import AddClassDialog from "./AddClassDialog";
 import Topbar from "../reusables/Topbar";
 
@@ -26,7 +30,7 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1)
   },
   gridContainer: {
-    paddingTop: theme.spacing(3)
+    paddingTop: "100px"
   },
   root: {
     flexGrow: 1
@@ -37,33 +41,48 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.text.secondary
   }
 }));
-let socket;
+
 export default function ClassLanding() {
   let token = sessionStorage.getItem("token").split(" ")[1];
   const classes = useStyles();
-
-
-  const ENDPOINT = "localhost:5000";
-  useEffect(() => {
-    socket = io(ENDPOINT);
-    console.log(socket);
-  }, []);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const [verfication, setVerification] = useState(false);
+  const [userType, setUserType] = useState("3");
   // const theme = useTheme();
   // const matches = useMediaQuery(theme.breakpoints.up("md"));
+
+  const changeUserType = e => {
+    setUserType(e.data.user_type_id);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const checkValidations = () => {
+    console.log(sessionStorage.getItem("token").split(" ")[1]);
+    axios({
+      method: "post",
+      url: `/api/admin/check/designation`,
+      data: { token: sessionStorage.getItem("token").split(" ")[1] }
+    })
+      .then(data => {
+        setVerification(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     fetchUserData();
     fetchMentorClass();
-
-    axios.post({
-      method: "post",
-      url: `/api/my/classes`,
-      data: { token: sessionStorage.getItem("token").split(" ")[1] }
-    }).then(data=>{
-      console.log(data)
-    }).catch(err=>{
-      console.log(err)
-    })
+    checkValidations();
   }, []);
 
   const [tokState] = useState({ token: token });
@@ -77,6 +96,7 @@ export default function ClassLanding() {
       .then(data => {
         console.log(data);
         setData(data.data);
+        changeUserType(data);
       })
       .catch(err => {
         console.log(err);
@@ -84,6 +104,8 @@ export default function ClassLanding() {
   };
 
   const [classData, setClassData] = useState([]);
+
+  // get all class relative to this mentor(if user is verified)
   const fetchMentorClass = () => {
     axios({
       method: "post",
@@ -102,20 +124,25 @@ export default function ClassLanding() {
   return (
     <React.Fragment>
       <Topbar />
-
       {/* BODY */}
       <Container maxWidth="xl">
         <div className={classes.root}>
           <Grid container spacing={2} className={classes.gridContainer}>
-            <VerificationDialog />
+            {verfication ? (
+              <VerificationDialog changeUserType={changeUserType} />
+            ) : (
+              ""
+            )}
             <Grid item xs={12}>
-              <AddClassDialog
-                token={token}
-                fetchMentorClass={fetchMentorClass}
-              />
-              <FindClassDialog />
+              {userType === 3 ? (
+                <FindClassDialog />
+              ) : (
+                <AddClassDialog
+                  token={token}
+                  fetchMentorClass={fetchMentorClass}
+                />
+              )}
             </Grid>
-
             <Container maxWidth="lg" className={classes.flexy}>
               <CardPage classData={classData} data={data} />
             </Container>
