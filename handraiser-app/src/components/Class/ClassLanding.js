cc31c5674e1182bc3f22c04a15e609ba15102da1
JeffreyMolleno <jeffrey.molleno.21@gmail.com>
2020-02-05 02:33:23 +0800

    updates to verification

--- handraiser-app/src/components/Class/ClassLanding.js
+++ handraiser-app/src/components/Class/ClassLanding.js
@@ -1,12 +1,4 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
