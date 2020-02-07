a69b7c4e58076ba5a89239eb82e55aee676f2dee
JeffreyMolleno <jeffrey.molleno.21@gmail.com>
2020-02-07 17:29:33 +0800

    Fixed student queue bugs

--- handraiser-app/src/components/student/inQueue/inQueue_all.js
+++ handraiser-app/src/components/student/inQueue/inQueue_all.js
@@ -58,14 +58,6 @@ export default function InQueue(props) {
     <Paper style={{ maxHeight: "830px", overflow: "auto" }}>
       <List className={classes.root}>
         {concernsData.map((concern, index) => {
-          axios
-            .get(
-              `http://localhost:5000/api/userprofile/${concern.concern.user_id}`,
-              {}
-            )
-            .then(data => {
-              setImage(data.data[0].image);
-            });
           return (
             <div key={index}>
               <ListItem
@@ -76,7 +68,7 @@ export default function InQueue(props) {
                 }}
               >
                 <ListItemAvatar>
-                  <Avatar src={image}></Avatar>
+                  <Avatar src={concern.concern.image}></Avatar>
                 </ListItemAvatar>
                 <Menu
                   id="menu-appbar"
