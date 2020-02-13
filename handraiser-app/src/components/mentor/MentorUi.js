853a9ee2fdebecef8aea422d4016c27512f55a66
JeffreyMolleno <jeffrey.molleno.21@gmail.com>
2020-02-13 16:28:43 +0800

    added close button on handshake

--- handraiser-app/src/components/mentor/MentorUi.js
+++ handraiser-app/src/components/mentor/MentorUi.js
@@ -150,7 +150,11 @@ export default function Mentor() {
       });
   };
 
+  const [selection, setSelection] = useState(false);
+
   const handleBackQueue = rowData => {
+    setSelection(false);
+
     if (rowData.length === 0) {
       Swal.fire({
         icon: "error",
@@ -169,10 +173,8 @@ export default function Mentor() {
         concern_status: 2
       })
       .then(data => {
-
         socket.emit("handshake", { room: class_id });
 
-        
         axios
           .get(`http://localhost:5000/api/assisted_by/${data.data.user_id}`, {})
           .then(data => {
@@ -185,6 +187,7 @@ export default function Mentor() {
   };
 
   const rowDatahandler = rowData => {
+    setSelection(true);
     console.log(rowData);
     setConcernTitle(rowData.concern_title);
     setRowData(rowData);
@@ -256,56 +259,75 @@ export default function Mentor() {
           <Tabs rowDatahandler={rowDatahandler} class_id={class_id} />
         </Queue>
         <Help>
-          <Subject>
-            <TitleName
-              style={{
-                paddingTop: "25px"
-              }}
-            >
-              <Typography variant="h5">Concern: {concernTitle}</Typography>
-              <Typography
-                variant="subtitle2"
+          {selection ? (
+            <Subject>
+              <TitleName
                 style={{
-                  fontSize: "12.4px"
+                  paddingTop: "25px"
                 }}
               >
-                From: {name}
+                <Typography variant="h5">
+                  {selection
+                    ? `Concern: ${concernTitle}`
+                    : `Select any concern to interact`}
+                </Typography>
+                <Typography
+                  variant="subtitle2"
+                  style={{
+                    fontSize: "12.4px"
+                  }}
+                >
+                  From: ${name}
+                </Typography>
+              </TitleName>
+              <Option>
+                <div>
+                  <HelpIcon
+                    style={{
+                      fontSize: 30,
+                      color: "#c4c4c4",
+                      cursor: "pointer",
+                      color: "#372476"
+                    }}
+                  />
+                </div>
+                <div>
+                  <GroupIcon
+                    style={{
+                      fontSize: 30,
+                      color: "#c4c4c4",
+                      cursor: "pointer",
+                      color: "#372476"
+                    }}
+                  />
+                </div>
+                <div>
+                  <MoreVertIcon
+                    onClick={handleMenu}
+                    style={{
+                      fontSize: 30,
+                      color: "#c4c4c4",
+                      cursor: "pointer",
+                      color: "#372476"
+                    }}
+                  />
+                </div>
+              </Option>
+            </Subject>
+          ) : (
+            <div
+              style={{
+                height: "96px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Typography variant="h5" style={{color:'#bcbcbc'}}>
+                Select any concern to interact
               </Typography>
-            </TitleName>
-            <Option>
-              <div>
-                <HelpIcon
-                  style={{
-                    fontSize: 30,
-                    color: "#c4c4c4",
-                    cursor: "pointer",
-                    color: "#372476"
-                  }}
-                />
-              </div>
-              <div>
-                <GroupIcon
-                  style={{
-                    fontSize: 30,
-                    color: "#c4c4c4",
-                    cursor: "pointer",
-                    color: "#372476"
-                  }}
-                />
-              </div>
-              <div>
-                <MoreVertIcon
-                  onClick={handleMenu}
-                  style={{
-                    fontSize: 30,
-                    color: "#c4c4c4",
-                    cursor: "pointer",
-                    color: "#372476"
-                  }}
-                />
-              </div>
-            </Option>
-          </Subject>
+            </div>
+          )}
 
           {rowData.concern_status === 2 ? (
             <Handshake data={rowData} rowDatahandler={rowDatahandler} />
@@ -313,45 +335,59 @@ export default function Mentor() {
             ""
           )}
 
-          <Chatfield />
-
-          {rowData.concern_status === 2 ? (
-            ""
+          {selection ? (
+            <Chatfield />
           ) : (
-            <Message>
-              <Field>
-                <div
-                  style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "space-between",
-                    flexDirection: "column",
-                    width: "100%"
-                  }}
-                >
-                  <form onSubmit={sendMsg}>
-                    {/* <TextField
-                      id="outlined-textarea"
-                      multiline
-                      variant="outlined"
-                      fullWidth
-                      rows="3"
-                    /> */}
-                    <Input />
+            <div
+              style={{
+                height: "83vh",
+                width: "100%",
+                background: "#f5f5f5"
+              }}
+            ></div>
+          )}
 
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        marginTop: "15px"
-                      }}
-                    >
-                      <Send onClick={sendMsg}>SEND</Send>
-                    </div>
-                  </form>
-                </div>
-              </Field>
-            </Message>
+          {selection ? (
+            rowData.concern_status === 2 ? (
+              ""
+            ) : (
+              <Message>
+                <Field>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexWrap: "wrap",
+                      justifyContent: "space-between",
+                      flexDirection: "column",
+                      width: "100%"
+                    }}
+                  >
+                    <form onSubmit={sendMsg}>
+                      {/* <TextField
+                        id="outlined-textarea"
+                        multiline
+                        variant="outlined"
+                        fullWidth
+                        rows="3"
+                      /> */}
+                      <Input />
+
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "flex-end",
+                          marginTop: "15px"
+                        }}
+                      >
+                        <Send onClick={sendMsg}>SEND</Send>
+                      </div>
+                    </form>
+                  </div>
+                </Field>
+              </Message>
+            )
+          ) : (
+            ""
           )}
         </Help>
         <DetailPanel />
--- handraiser-app/src/components/mentor/reactives/Handshake.js
+++ handraiser-app/src/components/mentor/reactives/Handshake.js
@@ -5,7 +5,6 @@ import axios from "axios";
 import io from "socket.io-client";
 import { useHistory, useParams } from "react-router-dom";
 
-
 var jwtDecode = require("jwt-decode");
 
 export default function Handshake(props) {
@@ -17,9 +16,7 @@ export default function Handshake(props) {
 
   const ENDPOINT = "localhost:5000";
 
-
   let socket = io(ENDPOINT);
-
 
   useEffect(() => {
     socket = io(ENDPOINT);
@@ -29,7 +26,6 @@ export default function Handshake(props) {
       room: class_id,
       image: ""
     });
-
   });
 
   const accept = highdata => {
@@ -47,7 +43,7 @@ export default function Handshake(props) {
         props.rowDatahandler(data.data);
 
         console.log(highdata);
-        
+
         socket.emit("handshake", { room: highdata.class_id });
 
         axios
@@ -110,16 +106,37 @@ export default function Handshake(props) {
         <div className={classes.handshake_text_container}>
           <p className={classes.handshake_text_main}>Respond to request?</p>
         </div>
-        <div className={classes.triangle_aest} />
-        <div
-          className={classes.handshake_button_container}
-          onClick={() => {
-            accept(props.data);
-            // window.location.reload();
-          }}
-        >
-          <img className={classes.handshake_img} src={HandShakeImage} />
-          <p className={classes.handshake_text_btn}>ACCEPT</p>
+
+        <div className={classes.button_flex_container}>
+          <div className={classes.button_main_container_2}>
+            <div className={classes.triangle_aest} />
+            <div
+              className={classes.handshake_button_container}
+              onClick={() => {
+                accept(props.data);
+                // window.location.reload();
+              }}
+            >
+              <img className={classes.handshake_img} src={HandShakeImage} />
+              <p className={classes.handshake_text_btn}>ACCEPT</p>
+            </div>
+          </div>
+
+          <div className={classes.button_main_container}>
+            <div className={classes.triangle_aest_2} />
+            <div
+              className={classes.handshake_button_container_2}
+              onClick={() => {
+                accept(props.data);
+                // window.location.reload();
+              }}
+            >
+              {/* <img className={classes.handshake_img} src={HandShakeImage} /> */}
+              <p className={classes.handshake_text_btn_2}>X</p>
+
+              <p className={classes.handshake_text_btn}>CLOSE</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -127,6 +144,26 @@ export default function Handshake(props) {
 }
 
 const useStyles = makeStyles(theme => ({
+  button_flex_container: {
+    display: "flex",
+    height: "100%"
+  },
+  button_main_container: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: "2"
+  },
+  button_main_container_2: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    left: "25px",
+    zIndex: "1"
+  },
   handshake_main_btn: {
     display: "flex",
     justifyContent: "center",
@@ -142,7 +179,7 @@ const useStyles = makeStyles(theme => ({
     paddingBottom: "10px"
   },
   handshake_container: {
-    width: "500px",
+    width: "600px",
     height: "121px",
     display: "flex",
     justifyContent: "space-between",
@@ -169,6 +206,23 @@ const useStyles = makeStyles(theme => ({
     },
     zIndex: "1"
   },
+  handshake_button_container_2: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "100px",
+    cursor: "pointer",
+    background: "#F00A38",
+    "&:hover": {
+      background: "#c4032a"
+    },
+    "&:hover div": {
+      borderColor: "transparent #00eb41 transparent transparent"
+    },
+    zIndex: "1"
+  },
   handshake_text_container: {
     marginLeft: "95px"
   },
@@ -179,6 +233,13 @@ const useStyles = makeStyles(theme => ({
     fontSize: "0.8em",
     color: "white",
     fontWeight: "800"
+  },
+  handshake_text_btn_2: {
+    fontSize: "3em",
+    color: "white",
+    fontWeight: "800",
+    fontWeight: "200",
+
   },
   handshake_img: {
     backgroundSize: "cover",
@@ -194,7 +255,18 @@ const useStyles = makeStyles(theme => ({
     borderColor: "transparent #2FDC5F transparent transparent",
     position: "relative",
     top: "2px",
-    right: "-39px",
+    right: "-6px",
+    zIndex: "0"
+  },
+  triangle_aest_2: {
+    width: "0",
+    height: "0",
+    borderStyle: "solid",
+    borderWidth: "12.5px 25px 12.5px 0",
+    borderColor: "transparent #F00A38 transparent transparent",
+    position: "relative",
+    top: "2px",
+    right: "-6px",
     zIndex: "0"
   }
 }));
